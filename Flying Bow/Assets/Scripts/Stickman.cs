b13185462a45c713e9cc5c248f852a49300b40cf
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-08-04 16:54:22 +0500

    Бета

--- Flying Bow/Assets/Scripts/Stickman.cs
+++ Flying Bow/Assets/Scripts/Stickman.cs
@@ -8,16 +8,34 @@ public class Stickman : MonoBehaviour
     [SerializeField] private Material _dieMaterial;
     [SerializeField] private SkinnedMeshRenderer _renderer;
 
-    private void OnTriggerStay(Collider other)
+    private Rigidbody _body;
+    private bool _applicatedVelocity = false;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent<Arrow>(out Arrow arrow))
         {
             _animation.enabled = false;
-            _renderer.material = _dieMaterial;
-            arrow.enabled = false;
             arrow.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
             arrow.GetComponent<Rigidbody>().isKinematic = true;
+            arrow.enabled = false;
             arrow.transform.SetParent(transform);
+            _renderer.material = _dieMaterial;
+        }
+    }
+
+    private void Update()
+    {
+        if(_animation.isActiveAndEnabled == false && _applicatedVelocity == false)
+        {
+            _applicatedVelocity = true;
+            _body.velocity = Vector3.zero;
+            _body.angularVelocity = Vector3.zero;
         }
     }
 }
